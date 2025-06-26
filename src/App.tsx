66f72fd3ba4479ac2532865508c742ba9ef3f949
sLix1337x <1337slx@gmail.com>
2025-06-26 16:06:37 +0200

    Fix: Switch from BrowserRouter to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, HashRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 // Page imports
@@ -82,7 +82,7 @@ const App = () => (
     <TooltipProvider>
       <HelmetProvider>
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <SmoothScroll>
             <div className="flex flex-col min-h-[calc(100vh-5rem)]">
               <header className="flex-shrink-0">
@@ -95,7 +95,7 @@ const App = () => (
               </div>
             </div>
           </SmoothScroll>
-        </BrowserRouter>
+        </HashRouter>
       </HelmetProvider>
     </TooltipProvider>
   </QueryClientProvider>
